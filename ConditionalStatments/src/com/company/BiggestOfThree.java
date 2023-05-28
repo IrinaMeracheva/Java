583b8b976ffbddc9d959f8babbc0b7c78fb65819
Irina Meracheva <irinapeeva90@gmail.com>
2023-05-28 23:22:45 +0200

    Add new task in Condition Statements

--- ConditionalStatments/src/com/company/BiggestOfThree.java
+++ ConditionalStatments/src/com/company/BiggestOfThree.java
@@ -1,4 +1,13 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class BiggestOfThree {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        System.out.println(Math.max(a, Math.max(b, c)));
+    }
 }
