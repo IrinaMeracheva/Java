99f3e70403d6412d2821ea18288c20463c66adf1
Irina Meracheva <irinapeeva90@gmail.com>
2023-06-08 13:51:17 +0200

    Add new task in Arrays

--- Arrays/src/com/company/SortNumbers.java
+++ Arrays/src/com/company/SortNumbers.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class SortNumbers {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        String[] elements = scanner.nextLine().split(", ");
+        Integer[] numbers = new Integer[elements.length];
+
+        for (int i = 0; i < elements.length; i++) {
+            numbers[i] = Integer.parseInt(elements[i]);
+        }
+        Arrays.sort(numbers, Collections.reverseOrder());
+        String output = Arrays.toString(numbers).replace("[", "").replace("]", "");
+        System.out.println(output);
     }
 }
