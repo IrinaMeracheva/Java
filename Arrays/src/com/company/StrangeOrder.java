f83d7a8059682e3d448601ee11a8a4bcb1989c1e
Irina Meracheva <irinapeeva90@gmail.com>
2023-06-09 12:39:52 +0200

    Add new task in Arrays

---
+++ Arrays/src/com/company/StrangeOrder.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class StrangeOrder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] input = scanner.nextLine().split(",");
+        int[] numbers = new int[input.length];
+        int[] result = new int[numbers.length];
+        int position = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = Integer.parseInt(input[i]);
+            if (numbers[i] < 0) {
+                result[position++] = numbers[i];
+            }
+        }
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == 0) {
+                result[position++] = numbers[i];
+            }
+        }
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > 0) {
+                result[position++] = numbers[i];
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int number:result) {
+            sb.append(Integer.toString(number)).append(",");
+        }
+        System.out.println(sb.toString().substring(0, sb.length() - 1));
+    }
+}
